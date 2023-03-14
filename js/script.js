9045bc9d37a1f59ac93ddf36008d054018b6e842
elijah <iljakononenko3@gmail.com>
2023-03-14 20:00:39 +0100

    last changes (except links and phone)

--- js/script.js
+++ js/script.js
@@ -10,8 +10,31 @@ $(document).ready(function () {
     $("#gallery").append(element);
   }
 
+  const myModalAlternative = new bootstrap.Modal("#modal", {});
+
   $("#free_estimate").on("submit", function (e) {
     e.preventDefault();
+    myModalAlternative.show();
+
+    let name = $("#name").val();
+    let phone = $("#phone").val();
+    let zip = $("#zip").val();
+
+    $("#name").val("");
+    $("#phone").val("");
+    $("#zip").val("");
+
+    Email.send({
+      SecureToken: "1bd4ec59-a493-466f-931a-39ea36c9b5cb",
+      To: "iljakononenko2@gmail.com",
+      From: "iljakononenko3@gmail.com",
+      Subject: "New Order!",
+      Body: `Name: ${name}<br>Phone: ${phone}<br>Zip: ${zip}`,
+    }).then((message) => console.log(message));
+
+    console.log(name);
+    console.log(phone);
+    console.log(zip);
   });
 
   $("#header-fixer").css("paddingTop", $("nav.fixed-top").outerHeight());
@@ -20,3 +43,5 @@ $(document).ready(function () {
     $("#header-fixer").css("paddingTop", $("nav.fixed-top").outerHeight());
   });
 });
+
+// data-bs-target="#modal" data-bs-toggle="modal"
