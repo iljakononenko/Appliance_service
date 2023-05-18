e7dd2bc868f97fe44dbf1756cb8316fe6f0917f3
iljakononenko <iljakononenko3@gmail.com>
2023-05-18 02:02:28 +0200

    add comments slider and fixed button in header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,114 @@
+let opinions = [
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness, Value",
+    text: "This young man was early for the appointment, very knowledgeable and proficient. I highly recommend this service!",
+    service: "Dishwasher repair",
+    author: "Phyllis Owen",
+  },
+  {
+    positive: "",
+    text: "Great customer service!! David was so friendly and professional!! Definitely recommend this company and will be using them in the future!!!",
+    service: "",
+    author: "Amanda Hinkle",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness, Value",
+    text: "David was awesome repairing my washer!! When I called they were so responsive got in right away and he showed up exactly on time was very Professional and did a great job! Would definitely use them for any services in the future!",
+    service: "",
+    author: "Maitee Miles Artist/Owner",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness",
+    text: "David was prompt, very professional and extremely knowledgeable and nice. Thank you for your service. ",
+    service: "Washer/dryer repair",
+    author: "Richard Reid",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness, Value",
+    text: "David, was professional and very knowledgeable he repair our freezer. Refrigerator  and dishwasher. In a timely manner. We would recommend and use SmartWay again! Thank you David!",
+    service: "Refrigerator repair, Dishwasher repair, Freezer repair",
+    author: "Eugenia Abohasira",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality",
+    text: "Very professional company.  David was on time, courteous, and explained what he was doing and why.  He quickly assessed the problem.  This is the 3rd time I've used this company and would definitely use them again.",
+    service: "Stove, cooktop & oven repair",
+    author: "Bobbie Williams",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness, Value",
+    text: "David the Technician was very professional and knowledgeable about what he was doing. He also explained the issue that I was having as he worked on my dishwasher. I will definitely use this company again . Great job David my husband and I appreciate the service you provided us today.☺️",
+    service: "Washing machine repair",
+    author: "Lisa Caruth",
+  },
+  {
+    positive: "",
+    text: "On time polite and did a great job. David was very personal and professional we'll definitely use this company again. ",
+    service: "Dishwasher repair",
+    author: "Kathy Saffell",
+  },
+  {
+    positive: "",
+    text: "David is the man! Very honest and on time! Took his time and answered all my questions! Very very happy with their services! Very reasonable and trustworthy! I would choose them again every time! Thank you!",
+    service: "",
+    author: "Mark S",
+  },
+  {
+    positive: "Professionalism, Punctuality",
+    text: "David figured out the issue in about 15 minutes and returned the next day with parts and had it done in about 20 minutes. Quick and easy.",
+    service: "Washer/dryer repair",
+    author: "Kevin Terry",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness, Value",
+    text: "David did an excellent job replacing our ice maker. Very professional, knowledgeable and courteous. I would highly recommend David and Smartway for your appliance repair needs. JA. ",
+    service: "Refrigerator/freezer repair",
+    author: "James Alexander",
+  },
+  {
+    positive: "Professionalism, Punctuality, Responsiveness",
+    text: "David was very thorough in finding the issue with my machine. He answered any questions I had about the repair and explained the process to me. He is an asset to your company.",
+    service: "Washing machine repair",
+    author: "Pennie Grayson",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness, Value",
+    text: "David went directly to the problem, ordered the correct parts and installed them when they arrived. He kept me informed through the whole process. We will use them again on all our broken appliances.",
+    service: "Refrigerator/freezer repair",
+    author: "Gary Turley",
+  },
+  {
+    positive: "",
+    text: "Thank you  David",
+    service: "",
+    author: "Pinar Temiz",
+  },
+  {
+    positive: "",
+    text: "DAvid did an excellent job fixing our refrigerator. He was neat, professional, and quick.",
+    service: "Refrigerator/freezer repair",
+    author: "Barry Hickey",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness, Value",
+    text: "David was punctual, thorough and honest. Nice to know there are still good workmen out there!",
+    service: "Washing machine repair",
+    author: "Laura P",
+  },
+  {
+    positive: "",
+    text: "David came out promptly and diagnosed the problem. He gave me a quote and the approximate time he would return with the necessary parts to fix the problem. He returned on time texting me on his on his way back. He fixed the problem plus another issue he saw. He was very knowledgeable and professional. I am very satisfied with the work and service.",
+    service: "",
+    author: "Donte Jones",
+  },
+  {
+    positive: "Professionalism, Punctuality, Quality, Responsiveness, Value",
+    text: "Great experience! Very quick response and David, the tech was on time. Our washer was fixed  for a reasonable fee on the day I called. I would use SmartWay again and recommend them to others.",
+    service: "Washer/dryer repair",
+    author: "Neal Beaty",
+  },
+];
+
 $(document).ready(function () {
   for (let i = 1; i < 47; i++) {
     let element = `
@@ -10,7 +121,59 @@ $(document).ready(function () {
     $("#gallery").append(element);
   }
 
-  $(".owl-carousel").owlCarousel({
+  for (let { positive, text, service, author } of opinions) {
+    let opinion_element = `
+  <div class="opinion-box text-center">
+          <div class="opinion-stars d-flex justify-content-center" style="height: 22px; margin-bottom: 16px;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.44 19"><polygon fill="#fdd663" points="10,15.27 16.18,19 14.54,11.97 20,7.24 12.81,6.63 10,0 7.19,6.63 0,7.24 5.46,11.97 3.82,19"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.44 19"><polygon fill="#fdd663" points="10,15.27 16.18,19 14.54,11.97 20,7.24 12.81,6.63 10,0 7.19,6.63 0,7.24 5.46,11.97 3.82,19"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.44 19"><polygon fill="#fdd663" points="10,15.27 16.18,19 14.54,11.97 20,7.24 12.81,6.63 10,0 7.19,6.63 0,7.24 5.46,11.97 3.82,19"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.44 19"><polygon fill="#fdd663" points="10,15.27 16.18,19 14.54,11.97 20,7.24 12.81,6.63 10,0 7.19,6.63 0,7.24 5.46,11.97 3.82,19"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.44 19"><polygon fill="#fdd663" points="10,15.27 16.18,19 14.54,11.97 20,7.24 12.81,6.63 10,0 7.19,6.63 0,7.24 5.46,11.97 3.82,19"/></svg>
+          </div>
+          ${
+            positive
+              ? "<p><span class='fw-bold'>Positive: </span>" + positive + "</p>"
+              : ""
+          }
+          <p>${text}</p>
+          ${
+            service
+              ? "<p><span class='fw-bold'>Service: </span>" + service + "</p>"
+              : ""
+          }
+          <p class="fw-bold">${author}</p>
+        </div>
+        `;
+    $("#opinions").append(opinion_element);
+  }
+
+  $("#opinions").owlCarousel({
+    loop: true,
+    stagePadding: 60,
+    dots: true,
+    dotsEach: 2,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 0,
+      },
+      768: {
+        items: 1,
+        stagePadding: 0,
+      },
+      1024: {
+        items: 1,
+        stagePadding: 0,
+      },
+      1200: {
+        items: 4,
+        stagePadding: 0,
+      },
+    },
+  });
+
+  $("#gallery").owlCarousel({
     loop: true,
     stagePadding: 60,
     dots: true,
